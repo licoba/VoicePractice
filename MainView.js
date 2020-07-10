226fb15a0dbb4af9a81522baf6d49e5140c9683b
licoba <licoba@163.com>
2020-07-10 19:00:48 +0800

    接口已经跑通了

--- MainView.js
+++ MainView.js
@@ -42,7 +42,8 @@ export default class MainView extends Component {
             recordState: RecordState.FINISHED,
             error: '',
             responseText: '提交评测后查看结果',
-            submitText: 'Can you hear me?'
+            submitText: 'Can you hear me?',
+            scoreText: '',
         };
     }
 
@@ -124,206 +125,218 @@ export default class MainView extends Component {
             ]
             );
 
-            let responseJson = await response.text();
+            let responseJson = await response.json();
             console.log("请求返回的结果", responseJson)
-        } catch (error) {
-            console.error("请求返回error", error);
+            if (responseJson.score) { 
+            this.setState({
+                scoreText: responseJson.score
+            })
+        }else {
+            this.setState({
+                scoreText: ""
+            })
         }
+        this.setState({
+            responseText: response.text()
+        })
+    } catch(error) {
+        console.error("请求返回error", error);
     }
+}
 
 
-    componentWillUnmount() {
-        clearInterval(this._progressInterval);
-    }
+componentWillUnmount() {
+    clearInterval(this._progressInterval);
+}
 
-    render() {
-        return (
-            <View style={{ flex: 1, flexDirection: 'column' }}>
-                <View style={{ flex: 6, flexDirection: 'column', alignItems: 'center', }}>
-                    <Input
-                        placeholder='输入文本'
-                        leftIcon={{ type: 'font-awesome', name: 'hand-o-right' }}>{this.state.submitText}</Input>
-                    <Text style={{ position: 'absolute', fontSize: 14, top: 55, left: 20, textAlign: 'left' }}>评测结果</Text>
-                    <ScrollView style={{ flex: 1, marginTop: 4, marginBottom: 20, marginLeft: 20, marginRight: 20, }}>
-                        <Text style={{
-                            flex: 1,
-                            fontSize: 18,
-                            color: '#808080',
-                            textAlign: 'left'
-                        }}>{this.state.responseText}</Text>
-                    </ScrollView>
-                    <Button buttonStyle={{ width: 500, height: 50 }} onPress={() => {
-                        this._submitTest();
-                    }}
-                        title="提交评测" loading={false}
-                        disabled={this.state.recordState == RecordState.FINISHED ? false : true} />
-                </View>
-                <View style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    backgroundColor: '#22aaaaaa',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                }}>
+render() {
+    return (
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+            <View style={{ flex: 6, flexDirection: 'column', alignItems: 'center', }}>
+                <Input
+                    placeholder='输入文本'
+                    leftIcon={{ type: 'font-awesome', name: 'hand-o-right' }}>{this.state.submitText}</Input>
+                <Text style={{ position: 'absolute', color: 'red', fontSize: 20, top: 55, left: 20, textAlign: 'left' }}>{this.state.scoreText === "" ? "" : "得分：" + this.state.scoreText}</Text>
+                <ScrollView style={{ flex: 0.6, marginTop: 6, marginBottom: 16, marginLeft: 20, marginRight: 20, }}>
                     <Text style={{
-                        position: 'absolute',
-                        color: '#DC143C',
-                        fontSize: 20,
-                        top: 0,
-                        left: 20,
-                        right: 20,
-                        textAlign: 'center'
-                    }}>{this.state.error}</Text>
-                    <Button title='开始录音' onPress={() => {
-                        this._toggleRecord();
-                    }}
-                        disabled={this.state.recordState == RecordState.RECORDING ? true : false}
-                        buttonStyle={{ width: 100, height: 50, color: '#4682B4', marginLeft: 20 }}></Button>
-                    <Button title='结束录音' onPress={() => this._stop()}
-                        disabled={this.state.recordState == RecordState.RECORDING ? false : true}
-                        buttonStyle={{ width: 100, height: 50, backgroundColor: '#733' }}></Button>
-                    <Button title='播放录音' onPress={() => this._playPause()}
-                        disabled={this.state.recordState == RecordState.FINISHED ? false : true} buttonStyle={{
-                            width: 100,
-                            height: 50,
-                            backgroundColor: '#008000',
-                            marginRight: 20
-                        }}></Button>
-                </View>
+                        fontSize: 18,
+                        color: '#808080',
+                        textAlign: 'left',
+                        fontFamily: ''
+                    }}>{this.state.responseText}</Text>
+                </ScrollView>
+                <Button buttonStyle={{ width: 500, height: 50 }} onPress={() => {
+                    this._submitTest();
+                }}
+                    title="提交评测" loading={false}
+                    disabled={this.state.recordState == RecordState.FINISHED ? false : true} />
             </View>
-        );
+            <View style={{
+                flex: 1,
+                flexDirection: 'row',
+                backgroundColor: '#22aaaaaa',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+            }}>
+                <Text style={{
+                    position: 'absolute',
+                    color: '#DC143C',
+                    fontSize: 20,
+                    top: 0,
+                    left: 20,
+                    right: 20,
+                    textAlign: 'center'
+                }}>{this.state.error}</Text>
+                <Button title='开始录音' onPress={() => {
+                    this._toggleRecord();
+                }}
+                    disabled={this.state.recordState == RecordState.RECORDING ? true : false}
+                    buttonStyle={{ width: 100, height: 50, color: '#4682B4', marginLeft: 20 }}></Button>
+                <Button title='结束录音' onPress={() => this._stop()}
+                    disabled={this.state.recordState == RecordState.RECORDING ? false : true}
+                    buttonStyle={{ width: 100, height: 50, backgroundColor: '#733' }}></Button>
+                <Button title='播放录音' onPress={() => this._playPause()}
+                    disabled={this.state.recordState == RecordState.FINISHED ? false : true} buttonStyle={{
+                        width: 100,
+                        height: 50,
+                        backgroundColor: '#008000',
+                        marginRight: 20
+                    }}></Button>
+            </View>
+        </View>
+    );
+}
+
+
+_stop() {
+    this._toggleRecord()
+}
+
+_playPause() {
+    this.player.play((err) => {
+        if (err) {
+            this.setState({
+                error: err.message
+            });
+        } else {
+            console.log('播放/暂停')
+        }
+    });
+}
+
+_toggleRecord() {
+
+    if (this.player) {
+        this.player.destroy();
     }
 
-
-    _stop() {
-        this._toggleRecord()
+    let recordAudioRequest;
+    if (Platform.OS == 'android') {
+        recordAudioRequest = this._requestRecordAudioPermission();
+    } else {
+        recordAudioRequest = new Promise(function (resolve, reject) {
+            resolve(true);
+        });
     }
 
-    _playPause() {
-        this.player.play((err) => {
+    recordAudioRequest.then((hasPermission) => {
+        if (!hasPermission) {
+            this.setState({
+                error: 'Record Audio Permission was denied'
+            });
+            return;
+        }
+
+        this.setState({ recordState: RecordState.RECORDING });
+        this.recorder.toggleRecord((err, stopped) => {
             if (err) {
                 this.setState({
                     error: err.message
                 });
-            } else {
-                console.log('播放/暂停')
+            }
+            if (stopped) {
+                this._reloadPlayer();
+                this._reloadRecorder();
+                this.setState({ recordState: RecordState.FINISHED })
             }
         });
+    });
+}
+
+_reloadRecorder() {
+    if (this.recorder) {
+        this.recorder.destroy();
     }
 
-    _toggleRecord() {
+    this.recorder = new Recorder(filename, {
+        bitrate: 256000,
+        channels: 1,
+        sampleRate: 44100,
+        quality: 'max',
+        format: 'mp3'
+    });
+    // this.recorder.prepare((err) => {
+    //     if (err) {
+    //         console.log('error at prepareRecorder():', err);
+    //     } else {
+    //         this.fsPath = this.recorder.fsPath;
+    //     }
+    // });
+}
 
-        if (this.player) {
-            this.player.destroy();
-        }
-
-        let recordAudioRequest;
-        if (Platform.OS == 'android') {
-            recordAudioRequest = this._requestRecordAudioPermission();
+_reloadPlayer() {
+    if (this.player) {
+        this.player.destroy();
+    }
+    this.player = new Player(filename, {
+        autoDestroy: false
+    }).prepare((err) => {
+        if (err) {
+            console.log('error at reloadPlayer():');
+            console.log(err);
         } else {
-            recordAudioRequest = new Promise(function (resolve, reject) {
-                resolve(true);
-            });
+            this.player.looping = false
         }
+        this._updateState();
+    });
 
-        recordAudioRequest.then((hasPermission) => {
-            if (!hasPermission) {
-                this.setState({
-                    error: 'Record Audio Permission was denied'
-                });
-                return;
-            }
+    this.player.on('ended', () => {
+        console.log('播放结束')
+        this._updateState();
+    });
+    this.player.on('pause', () => {
+        console.log('播放暂停')
+        this._updateState();
+    });
+}
 
-            this.setState({ recordState: RecordState.RECORDING });
-            this.recorder.toggleRecord((err, stopped) => {
-                if (err) {
-                    this.setState({
-                        error: err.message
-                    });
-                }
-                if (stopped) {
-                    this._reloadPlayer();
-                    this._reloadRecorder();
-                    this.setState({ recordState: RecordState.FINISHED })
-                }
-            });
-        });
-    }
+_updateState(err) {
 
-    _reloadRecorder() {
-        if (this.recorder) {
-            this.recorder.destroy();
-        }
+}
 
-        this.recorder = new Recorder(filename, {
-            bitrate: 256000,
-            channels: 1,
-            sampleRate: 44100,
-            quality: 'max',
-            format: 'mp3'
-        });
-        // this.recorder.prepare((err) => {
-        //     if (err) {
-        //         console.log('error at prepareRecorder():', err);
-        //     } else {
-        //         this.fsPath = this.recorder.fsPath;
-        //     }
-        // });
-    }
-
-    _reloadPlayer() {
-        if (this.player) {
-            this.player.destroy();
-        }
-        this.player = new Player(filename, {
-            autoDestroy: false
-        }).prepare((err) => {
-            if (err) {
-                console.log('error at reloadPlayer():');
-                console.log(err);
-            } else {
-                this.player.looping = false
-            }
-            this._updateState();
-        });
-
-        this.player.on('ended', () => {
-            console.log('播放结束')
-            this._updateState();
-        });
-        this.player.on('pause', () => {
-            console.log('播放暂停')
-            this._updateState();
-        });
-    }
-
-    _updateState(err) {
-
-    }
-
-    async _requestRecordAudioPermission() {
-        // console.log('请求录音权限')
-        try {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-                {
-                    title: 'Microphone Permission',
-                    message: 'ExampleApp needs access to your microphone to test react-native-audio-toolkit.',
-                    buttonNeutral: 'Ask Me Later',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                },
-            );
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (err) {
-            console.error(err);
+async _requestRecordAudioPermission() {
+    // console.log('请求录音权限')
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+            {
+                title: 'Microphone Permission',
+                message: 'ExampleApp needs access to your microphone to test react-native-audio-toolkit.',
+                buttonNeutral: 'Ask Me Later',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+            },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            return true;
+        } else {
             return false;
         }
+    } catch (err) {
+        console.error(err);
+        return false;
     }
+}
 
 
 }
--- index.js
+++ index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry,Text } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+
+import { name as appName } from './app.json';
+// import { Button, Text } from 'react-native-elements';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// if (Platform.OS === 'android') {
+//     const oldRender = Text.render;
+//     Text.render = function (...args) {
+//         const origin = oldRender.call(this, ...args);
+//         return React.cloneElement(origin, {
+//             style: [origin.props.style, { fontFamily: '' }],
+//         });
+//     };
+// }
